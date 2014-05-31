decb23272a528ba5bbb44995f434652103fa62ac
bigman <mralex7788@gmail.com>
2014-05-31 14:43:40 +0800

    bigman

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define YEScoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "YEScoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
